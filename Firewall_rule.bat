6ac5a6857f04fcd6df71d00fdade1c16f93d24af
M <mohsenghasemy608@gmail.com>
2025-10-08 02:30:12 +0330

    firewall

--- Firewall_rule.bat
+++ Firewall_rule.bat
@@ -1,6 +1,6 @@
 :: For batch firewall block of all the exe files within a program's folder
 
-for %%G in ("C:\Program Files (x86)\NewSoftware's\Folder Lock\*.exe") do (
+for %%G in ("C:\Program Files\Adobe\Adobe Premiere Pro 2025\*.exe") do (
 
 netsh advfirewall firewall add rule name="Folder Lock Blocked With Batchfile %%G" dir=in action=block program="%%G" enable=yes profile=any
 netsh advfirewall firewall add rule name="Folder Lock Blocked With Batchfile %%G" dir=out action=block program="%%G" enable=yes profile=any
